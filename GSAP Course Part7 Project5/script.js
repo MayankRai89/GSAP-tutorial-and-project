cdd0fb9c9e98e96ca8e8ea583ee2f1fa20ddc87f
Mayank Rai <149506720+MayankRai89@users.noreply.github.com>
2026-02-12 23:13:29 +0530

    Add initial project files including HTML, CSS, and JavaScript for GSAP animations

---
+++ GSAP Course Part7 Project5/script.js
@@ -0,0 +1,126 @@
+function page1Animation() {
+  var tl = gsap.timeline({ defaultEase: "power2.out" });
+
+  tl.from("nav h1, nav h4, nav button", {
+    y: -40,
+    opacity: 0,
+    duration: 0.6,
+    stagger: 0.15,
+  });
+
+  tl.from(
+    ".centre-part1 h1",
+    {
+      x: -200,
+      opacity: 0,
+      duration: 0.6,
+    },
+    "-=0.3",
+  );
+
+  tl.from(
+    ".centre-part1 p",
+    {
+      x: -100,
+      opacity: 0,
+      duration: 0.5,
+    },
+    "-=0.3",
+  );
+
+  tl.from(
+    ".centre-part1 button",
+    {
+      // y: 20,
+      opacity: 0,
+      duration: 0.4,
+    },
+    "-=0.2",
+  );
+
+  tl.from(
+    ".centre-part2 img",
+    {
+      scale: 0.9,
+      opacity: 0,
+      duration: 0.6,
+    },
+    "-=0.8",
+  );
+
+  tl.from(
+    ".section1bottom img",
+    {
+      opacity: 0,
+      y: 30,
+      duration: 0.5,
+      stagger: 0.12,
+    },
+    "-=0.3",
+  );
+}
+page1Animation();
+
+function page2Animation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  var tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".container",
+      start: "top 70%",
+      end: "top 30%",
+      scrub: 1,
+    },
+  });
+
+  tl2.from(".services", {
+    y: -30,
+    opacity: 0,
+  });
+  tl2.from(".container", {
+    y: 50,
+    opacity: 0,
+    duration: 0.5,
+  });
+
+  tl2.from(
+    ".elem.line1.left",
+    {
+      x: -200,
+      opacity: 0,
+      duration: 0.6,
+    },
+    "anim",
+  );
+
+  tl2.from(
+    ".elem.line1.right",
+    {
+      x: 200,
+      opacity: 0,
+      duration: 0.6,
+    },
+    "anim",
+  );
+
+  tl2.from(
+    ".elem.line2.left",
+    {
+      x: -200,
+      opacity: 0,
+      duration: 0.6,
+    },
+    "anim2",
+  );
+
+  tl2.from(
+    ".elem.line2.right",
+    {
+      x: 200,
+      opacity: 0,
+      duration: 0.6,
+    },
+    "anim2",
+  );
+}
+page2Animation();
